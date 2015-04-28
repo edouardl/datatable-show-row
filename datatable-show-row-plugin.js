9851f6e39ecf1d751e81fc54570b7280706780e7
edouardl <edouard.labre@gmail.com>
2015-04-28 09:58:37 +0200

    Update datatable-show-row-plugin.js
    plugin return the table row object

--- datatable-show-row-plugin.js
+++ datatable-show-row-plugin.js
@@ -24,12 +24,15 @@ $.fn.dataTable.Api.register('row().r_show()', function() {
     var new_row_index = this.index();
     // Row position
     var row_position = this.table().rows()[0].indexOf( new_row_index );
-    // Already on right page
+    // Already on right page ?
     if( row_position >= page_info.start && row_position < page_info.end ) {
-            return;
+        // Return row object
+        return this;
     }
     // Find page number
     var page_to_display = Math.floor( row_position / this.table().page.len() );
     // Go to that page
     this.table().page( page_to_display ).draw( false );
+    // Return row object
+    return this;
 });
